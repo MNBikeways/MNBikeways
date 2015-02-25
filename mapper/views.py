a3c67764351307325ab7286629f8904c3baed728
boydjohnson <johnson.boyd@gmail.com>
2015-02-25 16:35:05 -0600

    Cleaned up views.py

--- mapper/views.py
+++ mapper/views.py
@@ -17,7 +17,6 @@ class OverpassApiAjax(View):
 
     def get(self,request,*args,**kwargs):
         api = overpass.API()
-        # OSM EPSG is 900913
         LatLonString = str(request.GET.get('lat', '42')) + "," + str(request.GET.get('lon', '-92')) + ")"
         r = api.Get('way(around:2000,' + LatLonString + "[bicycle=yes]", asGeoJSON=True)
         return HttpResponse(geojson.dumps(r), content_type="application/json; charset='utf-8'")
