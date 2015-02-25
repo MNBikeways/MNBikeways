5dee01eb925e39d28ee69401f147a3da514b2843
boydjohnson <johnson.boyd@gmail.com>
2015-02-25 12:38:50 -0600

    Using json.loads and json.dumps to get rid of u'

--- mapper/views.py
+++ mapper/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView, View
 from django.http import HttpResponse
 import overpass
-
+import json
 
 
 
@@ -21,4 +21,4 @@ class OverpassApiAjax(View):
         r = api.Get('way(around:1000,' + str(request.GET.get('lat', '42')) + "," + str(
             request.GET.get('lon', '-92')) + ")" + "[bicycle=yes];")
 
-        return HttpResponse(str(r['elements']), content_type="application/json; charset='utf-8'")
+        return HttpResponse(json.dumps(json.loads(r['elements']))), content_type="application/json; charset='utf-8'")
