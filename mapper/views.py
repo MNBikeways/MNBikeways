ddf930a1bdba531e69e7a7ee4b564c776490f469
boydjohnson <johnson.boyd@gmail.com>
2015-03-04 18:07:21 -0600

    So when the frontend GETs from OSM endpoint the frontend will give center and boundingbox points and the backend will calculate the distance between these two points and will create query to Overpass API based on the distance

--- mapper/views.py
+++ mapper/views.py
@@ -3,7 +3,7 @@ from django.views.generic import TemplateView, View
 from django.http import HttpResponse
 import overpass
 import geojson
-
+from geographiclib.geodesic import Geodesic
 
 class MainPage(TemplateView):
 
@@ -18,6 +18,15 @@ class OverpassApiAjax(View):
     def get(self,request,*args,**kwargs):
         api = overpass.API()
         around = str(request.GET.get('around', '2500'))
-        LatLonString = str(request.GET.get('lat', '42')) + "," + str(request.GET.get('lon', '-92')) + ")"
+        lat0 = str(request.GET.get('lat', 42))
+        lon0 = str(request.GET.get('lon', -92))
+        lat1 = str(request.GET.get('lat1', 0))  # means that 0 is the default if 'lat1' is not included
+        lon1 = str(request.GET.get('lon1', 0))
+        if lat1 == '0' or lon1 == '0':
+            around = str(2000)
+        else:
+            l = Geodesic.WGS84.Inverse(float(lat0), float(lon0), float(lat1), float(lon1))
+            around = str(l['s12']) if l['s12'] < 6000 else str(6000)
+        LatLonString = lat0 + "," + lon0 + ")"
         r = api.Get('way(around:' + around + ',' + LatLonString + "[bicycle=yes]", asGeoJSON=True)
         return HttpResponse(geojson.dumps(r), content_type="application/json; charset='utf-8'")
